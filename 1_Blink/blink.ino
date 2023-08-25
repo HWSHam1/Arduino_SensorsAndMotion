72af26bb1dc7b3c1960d2b310d6f89c5c4f1ce6f
Sheena Shabana <71267717+SheenaShabana@users.noreply.github.com>
2023-08-25 09:37:39 +0100

    Update blink.ino

--- 1_Blink/blink.ino
+++ 1_Blink/blink.ino
@@ -5,24 +5,21 @@
  Arduino Experiments
  
  Blink
- Turns on an LED on for 1500 millisecond, then off for 1500 millisecond, repeatedly.
+ Turns on an LED ON for 1 second (1000 millisecond), then turn it OFF for 1 second (1000 millisecond), repeatedly.
   
 */
 
-int LEDpin = 13; //This declares the variable LEDpin as an integer.
-// LEDpin defines the output pin used by the Arduino to power the LED.
-
 // The setup() method runs once when you press reset or power on the board.
 void setup()
-{
-  pinMode(LEDpin, OUTPUT);    // initialize the digital LED port as an output
-}
+ {
+  pinMode(LED_BUILTIN, OUTPUT);    // initialize the LED Builtin port, which is port 13 for Arduino Uno, as an output
+ }
 
 // The loop() method runs over and over again as long as the Arduino is powered on.
 void loop()
-{
-  digitalWrite(LEDpin, HIGH); // turn the LED on (HIGH is the voltage level)
-  delay(1500);             // Wait for 1500 milliseconds
-  digitalWrite(LEDpin, LOW); // turn the LED off (LOW is the voltage level)
-  delay(1500);             // Wait for 1500 milliseconds
-}
+ {
+  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+  delay(1000);             // Wait for 1500 milliseconds
+  digitalWrite(LED_BUILTIN, LOW); // turn the LED off (LOW is the voltage level)
+  delay(1000);             // Wait for 1500 milliseconds
+ }
